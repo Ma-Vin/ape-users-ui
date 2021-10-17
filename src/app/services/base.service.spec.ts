938af40609fd2179c428232c0698b7b0baa76c61
Ma-Vin <ma.vin.github@gmail.com>
2021-10-17 20:26:47 +0200

    feature - add base service

---
+++ src/app/services/base.service.spec.ts
@@ -0,0 +1,36 @@
+import { TestBed } from '@angular/core/testing';
+import { ConfigService } from '../config/config.service';
+import { BaseService } from './base.service';
+
+describe('BaseService', () => {
+  let service: BaseService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+
+    const mockConfig =
+    {
+      clientId: 'ape.user.ui',
+      clientSecret: 'changeIt',
+      backendBaseUrl: '//localhost:8080/'
+    };
+    const fake = { getConfig: () => mockConfig };
+
+    service = new TestBaseService('TestBaseService', fake as ConfigService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('init should set client info', () => {
+    service.ngOnInit();
+
+    expect(service.clientId).toEqual('ape.user.ui');
+    expect(service.clientSecret).toEqual('changeIt');
+  });
+
+});
+
+
+class TestBaseService extends BaseService { }
---
+++ src/app/services/base.service.ts
@@ -0,0 +1,57 @@
+import { Injectable, OnInit } from '@angular/core';
+import { HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { ConfigService } from '../config/config.service';
+import { throwError } from 'rxjs';
+
+
+export const HTTP_URL_OPTIONS = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/x-www-form-urlencoded'
+  })
+};
+
+export const HTTP_JSON_OPTIONS = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export abstract class BaseService implements OnInit {
+
+  public clientId: string | undefined;
+  public clientSecret: string | undefined;
+
+  constructor(private serviceName: string, protected configService: ConfigService) {
+  }
+
+  ngOnInit(): void {
+    let config = this.configService.getConfig();
+    this.clientId = config?.clientId;
+    this.clientSecret = config?.clientSecret;
+  }
+
+  public handleError(error: HttpErrorResponse) {
+    console.error('Error occurred at Service ' + this.serviceName);
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    return throwError(
+      'Something bad happened; please try again later.');
+  };
+
+  protected getHttpUrlWithClientBasicAuthOptions() {
+    return {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': `Basic ${btoa(this.clientId + ":" + this.clientSecret)}`
+      })
+    };
+  }
+}
