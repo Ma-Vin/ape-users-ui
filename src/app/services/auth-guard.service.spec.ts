ecf1b9784a8358f993f07d68d057bacf5efc334e
Ma-Vin <ma.vin.github@gmail.com>
2021-10-24 19:21:30 +0200

    feature: add guard service for routing

---
+++ src/app/services/auth-guard.service.spec.ts
@@ -0,0 +1,64 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
+import { AppRoutingModule, LOGIN_PATH } from '../app-routing.module';
+import { ConfigService } from '../config/config.service';
+
+import { AuthGuardService } from './auth-guard.service';
+import { AuthService } from './auth.service';
+import { CryptoService } from './crypto.service';
+
+describe('AuthGuardService', () => {
+  let service: AuthGuardService;
+  let authService: AuthService;
+  let configService: ConfigService;
+  let cryptoService: CryptoService;
+  let router: Router;
+  let httpMock: HttpTestingController;
+  let http: HttpClient;
+
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({ imports: [AppRoutingModule, HttpClientTestingModule, AppRoutingModule] });
+    router = TestBed.inject(Router);
+    httpMock = TestBed.inject(HttpTestingController);
+    http = TestBed.inject(HttpClient);
+    configService = TestBed.inject(ConfigService);
+    cryptoService = TestBed.inject(CryptoService);
+    authService = TestBed.inject(AuthService);
+    service = TestBed.inject(AuthGuardService);
+  });
+
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+
+  it('canActivate - invalid user', fakeAsync(() => {
+    let authServiceSpy = spyOn(authService, 'hasValidUser').and.callFake(() => of(false));
+    let routerSpy = spyOn(router, 'navigate').and.callFake(() => new Promise<boolean>((resolve, reject) => resolve(true)));
+
+    service.canActivate().subscribe(data => expect(data).toBeFalse());
+
+    expect(authServiceSpy).toHaveBeenCalled();
+    expect(routerSpy).toHaveBeenCalledOnceWith([LOGIN_PATH]);
+
+    tick();
+  }));
+
+
+  it('canActivate - valid user', fakeAsync(() => {
+    let authServiceSpy = spyOn(authService, 'hasValidUser').and.callFake(() => of(true));
+    let routerSpy = spyOn(router, 'navigate').and.callFake(() => new Promise<boolean>((resolve, reject) => resolve(true)));
+
+    service.canActivate().subscribe(data => expect(data).toBeTrue());
+
+    expect(authServiceSpy).toHaveBeenCalled();
+    expect(routerSpy).not.toHaveBeenCalledWith([LOGIN_PATH]);
+
+    tick();
+  }));
+});
---
+++ src/app/services/auth-guard.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { LOGIN_PATH } from '../app-routing.module';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService {
+
+  constructor(public auth: AuthService, public router: Router) { }
+
+  canActivate(): Observable<boolean> {
+    return this.auth.hasValidUser().pipe(map(
+      data => {
+        if (data) {
+          console.debug('canActivate: true');
+          return true;
+        }
+        else {
+          this.router.navigate([LOGIN_PATH]);
+          console.debug('canActivate: false');
+          return false;
+        }
+      })
+    );
+  }
+}
