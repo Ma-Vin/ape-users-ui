ebd70a85633785fd7328e397bae14b382f8512bc
Ma-Vin <ma.vin.github@gmail.com>
2022-01-06 18:49:12 +0100

    feature: extend model to provide map and equal methods

---
+++ src/app/model/privilege-group.model.spec.ts
@@ -0,0 +1,86 @@
+import { PrivilegeGroup } from "./privilege-group.model";
+
+describe('PrivilegeGroup', () => {
+    const userId = 'BGAA00001';
+    const groupName = 'Base';
+    const description = 'Some Base Group';
+
+    let privilegeGroup: PrivilegeGroup;
+    let otherPrivilegeGroup: PrivilegeGroup;
+
+    beforeEach(() => {
+        privilegeGroup = new PrivilegeGroup(groupName, userId);
+        privilegeGroup.description = description;
+        privilegeGroup.validFrom = new Date(2021, 12, 1);
+        privilegeGroup.validTo = new Date(2022, 12, 1);
+
+        otherPrivilegeGroup = new PrivilegeGroup(groupName, userId);
+        otherPrivilegeGroup.description = description;
+        otherPrivilegeGroup.validFrom = new Date(2021, 12, 1);
+        otherPrivilegeGroup.validTo = new Date(2022, 12, 1);
+    });
+
+    it('should be created', () => {
+        expect(privilegeGroup).toBeTruthy();
+    });
+
+    it('equal - undefined', () => {
+        expect(privilegeGroup.equals(undefined)).toBeFalse();
+    });
+
+    it('equal - other type', () => {
+        expect(privilegeGroup.equals(Object.assign({}, privilegeGroup))).toBeFalse();
+    });
+
+    it('equal - identical object', () => {
+        expect(privilegeGroup.equals(privilegeGroup)).toBeTrue();
+    });
+
+    it('equal - PrivilegeGroup with identical values', () => {
+        expect(privilegeGroup.equals(otherPrivilegeGroup)).toBeTrue();
+    });
+
+    it('equal - PrivilegeGroup with identical values and undefines', () => {
+        privilegeGroup.description = undefined;
+        privilegeGroup.validFrom = undefined;
+        privilegeGroup.validTo = undefined;
+
+        otherPrivilegeGroup.description = undefined;
+        otherPrivilegeGroup.validFrom = undefined;
+        otherPrivilegeGroup.validTo = undefined;
+
+        expect(privilegeGroup.equals(otherPrivilegeGroup)).toBeTrue();
+    });
+
+    it('equal - PrivilegeGroup not equal identification', () => {
+        otherPrivilegeGroup.identification = otherPrivilegeGroup.identification.concat('_');
+        expect(privilegeGroup.identification).not.toEqual(otherPrivilegeGroup.identification);
+        expect(privilegeGroup.equals(otherPrivilegeGroup)).toBeFalse();
+    });
+
+    it('equal - PrivilegeGroup not equal groupName', () => {
+        otherPrivilegeGroup.groupName = otherPrivilegeGroup.groupName.concat('_');
+        expect(privilegeGroup.groupName).not.toEqual(otherPrivilegeGroup.groupName);
+        expect(privilegeGroup.equals(otherPrivilegeGroup)).toBeFalse();
+    });
+
+    it('equal - PrivilegeGroup not equal description', () => {
+        otherPrivilegeGroup.description = otherPrivilegeGroup.description?.concat('_');
+        expect(privilegeGroup.description).not.toEqual(otherPrivilegeGroup.description);
+        expect(privilegeGroup.equals(otherPrivilegeGroup)).toBeFalse();
+    });
+
+    it('map - should have equal values', () => {
+        let mappedPrivilegeGroup = PrivilegeGroup.map(privilegeGroup);
+
+        expect(mappedPrivilegeGroup).toBeInstanceOf(PrivilegeGroup);
+
+        expect(mappedPrivilegeGroup.identification).toEqual(privilegeGroup.identification);
+        expect(mappedPrivilegeGroup.description).toEqual(privilegeGroup.description);
+        expect(mappedPrivilegeGroup.validFrom).toEqual(privilegeGroup.validFrom);
+        expect(mappedPrivilegeGroup.validTo).toEqual(privilegeGroup.validTo);
+
+        expect(privilegeGroup.equals(mappedPrivilegeGroup)).toBeTrue();
+    });
+
+});
--- src/app/model/privilege-group.model.ts
+++ src/app/model/privilege-group.model.ts
@@ -1,9 +1,10 @@
 import { IAbstractGroup } from "./abstract-group.model";
+import { IEqualsAndIdentifiable } from "./equals-identifiable";
 
 export interface IPrivilegeGroup extends IAbstractGroup {
 }
 
-export class PrivilegeGroup implements IPrivilegeGroup {
+export class PrivilegeGroup implements IPrivilegeGroup, IEqualsAndIdentifiable {
 
     constructor(groupName: string, identification: string) {
         this.groupName = groupName;
@@ -16,4 +17,42 @@ export class PrivilegeGroup implements IPrivilegeGroup {
     validFrom: Date | undefined;
     validTo: Date | undefined;
 
+    /**
+     * Creates an new PrivilegeGroup and maps the given values to the new one
+     * @param base the structure which is to map to a PrivilegeGroup
+     * @returns the new created PrivilegeGroup instance
+     */
+    public static map(base: IPrivilegeGroup): PrivilegeGroup {
+        let result = new PrivilegeGroup(base.groupName, base.identification);
+
+        result.description = base.description;
+        result.validFrom = base.validFrom;
+        result.validTo = base.validTo;
+
+        return result;
+    }
+
+
+    public equals(other: any): boolean {
+        if (other == undefined) {
+            return false;
+        }
+        if (this === other) {
+            return true;
+        }
+        if (!(other instanceof PrivilegeGroup)) {
+            return false;
+        }
+
+        return this.identification == other.identification
+            && this.groupName == other.groupName
+            && this.description == other.description
+            && this.validFrom?.getTime() == other.validFrom?.getTime()
+            && this.validTo?.getTime() == other.validTo?.getTime();
+    }
+
+
+    getIdentification(): string {
+        return this.identification;
+    }
 }
