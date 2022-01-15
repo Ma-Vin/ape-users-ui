d0049a3d2f73c52d346258c51e8e706024036887
Ma-Vin <ma.vin.github@gmail.com>
2022-01-15 20:23:49 +0100

    refactor: remove not used code

--- src/app/components/list-detail/detail-button-row/detail-button-row.component.ts
+++ src/app/components/list-detail/detail-button-row/detail-button-row.component.ts
@@ -5,17 +5,11 @@ import { Component, Input, OnInit } from '@angular/core';
   templateUrl: './detail-button-row.component.html',
   styleUrls: ['./detail-button-row.component.less']
 })
-export class DetailButtonRowComponent implements OnInit {
+export class DetailButtonRowComponent {
   @Input() onAccept!: () => void;
   @Input() disableAccept!: () => boolean;
   @Input() onCancel!: () => void;
   @Input() onDelete!: () => void;
   @Input() disableDelete!: () => boolean;
   @Input() isNewObject!: boolean;
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
