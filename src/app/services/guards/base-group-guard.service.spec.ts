2840a47c8423cc012154148a3381a87880a483ac
Ma-Vin <ma.vin.github@gmail.com>
2022-01-27 20:47:07 +0100

    feat: create guard service for base groups

---
+++ src/app/services/guards/base-group-guard.service.spec.ts
@@ -0,0 +1,70 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { ConfigService } from '../../config/config.service';
+import { AdminService } from '../backend/admin.service';
+import { CommonGroupService } from '../backend/common-group.service';
+import { UserService } from '../backend/user.service';
+import { BaseGroupPermissionsService } from '../permissions/base-group-permissions.service';
+import { SelectionService } from '../util/selection.service';
+
+import { BaseGroupGuardService } from './base-group-guard.service';
+
+describe('BaseGroupGuardService', () => {
+  let service: BaseGroupGuardService;
+  let baseGroupPermissionsService: BaseGroupPermissionsService
+  let adminService: AdminService;
+  let userService: UserService;
+  let commonGroupService: CommonGroupService;
+  let configService: ConfigService;
+  let selectionService: SelectionService;
+  let httpMock: HttpTestingController;
+  let http: HttpClient;
+
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
+
+    httpMock = TestBed.inject(HttpTestingController);
+    http = TestBed.inject(HttpClient);
+    configService = TestBed.inject(ConfigService);
+    adminService = TestBed.inject(AdminService);
+    userService = TestBed.inject(UserService);
+    commonGroupService = TestBed.inject(CommonGroupService);
+    selectionService = TestBed.inject(SelectionService);
+    baseGroupPermissionsService = TestBed.inject(BaseGroupPermissionsService);
+
+    service = TestBed.inject(BaseGroupGuardService);
+  });
+
+
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+
+  /**
+   * canActivate
+   */
+  it('canActivate - allowed to get all base groups', fakeAsync(() => {
+    let isAllowedToGetAllBaseGroupsSpy = spyOn(baseGroupPermissionsService, 'isAllowedToGetAllBaseGroups').and.returnValue(true);
+
+    service.canActivate().subscribe(data => expect(data).toBeTrue());
+
+    expect(isAllowedToGetAllBaseGroupsSpy).toHaveBeenCalled();
+
+    tick();
+  }));
+
+
+  it('canActivate - not allowed to get all  base groups', fakeAsync(() => {
+    let isAllowedToGetAllBaseGroupsSpy = spyOn(baseGroupPermissionsService, 'isAllowedToGetAllBaseGroups').and.returnValue(false);
+
+    service.canActivate().subscribe(data => expect(data).toBeFalse());
+
+    expect(isAllowedToGetAllBaseGroupsSpy).toHaveBeenCalled();
+
+    tick();
+  }));
+});
---
+++ src/app/services/guards/base-group-guard.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { BaseGroupPermissionsService } from '../permissions/base-group-permissions.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BaseGroupGuardService {
+
+  constructor(private baseGroupPermissionService: BaseGroupPermissionsService) { }
+
+  canActivate(): Observable<boolean> {
+    return of(this.baseGroupPermissionService.isAllowedToGetAllBaseGroups());
+  }
+}
